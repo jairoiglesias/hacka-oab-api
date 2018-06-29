2000da8e6bf6f7617fb7a876c6bdf9a40db73649
Jairo Iglesias <jairohighwind@hotmail.com>
2018-06-29 07:35:14 -0300

    Nova rota para upload de arquivo no formato de imagem (fix 7)

--- routes/router_views.js
+++ routes/router_views.js
@@ -431,8 +431,8 @@ module.exports = function(app) {
 
         console.log('Enviando os dados de OCR para EndPoint do NLU/WKS para analise')
         // res.send('Finalizado com sucesso')
-        res.send(ocrData)
-
+        res.send(reqWKS)
+        
         reqWKS.ocr.forEach(function(ocrData, ocrIndex){
 
           var url = 'https://dokia77.mybluemix.net/process'
