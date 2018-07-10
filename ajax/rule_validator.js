567573c7bab1bc334029206ee94a2d3075aea7b6
Jairo Iglesias <jairohighwind@hotmail.com>
2018-07-09 21:27:27 -0300

    Mock dados solicitacao caso os parametros nao sejam passados (teste local)

--- ajax/rule_validator.js
+++ ajax/rule_validator.js
@@ -5,7 +5,7 @@ const { promisify } = require('util')
 const VALIDATOR_URL = 'https://validator-dokia.mybluemix.net/api/validator'
 const ID_RULE = "90ad561435df4489b29e9fa8b4540315"
 
-// let MOCK_SOLICITACAO = {"id_solicitacao_cliente":"54834708","0":"54834708","carteira":"Santander NPL1","1":"Santander NPL1","origem":"A","2":"A","agencia_fornecedor":"Ferreira e Chagas Advogados","3":"Ferreira e Chagas Advogados","devedor":"RONALDO CABRAL DUTRA,","4":"RONALDO CABRAL DUTRA,","nro_operacao_sistema":"8492206","5":"8492206","nro_operacao":"2290000172180-32-0424","6":"2290000172180-32-0424","setor":"PCJ","7":"PCJ","tipo":"GASTO","8":"GASTO","autorizacao":"","9":"","nro_despesa":"","10":"","data_gasto":"2018-01-24","11":"2018-01-24","tipo_gasto":"NC_Correios","12":"NC_Correios","moeda":"R$","13":"R$","valor":"19.70","14":"19.70","status_pagamento":"Carregado","15":"Carregado","data_aprovacao":"2018-02-14","16":"2018-02-14","id_juizo":"","17":"","id_reclamo":"\r\n","18":"\r\n","cnpj":"22222222222","19":"22222222222"}
+let MOCK_SOLICITACAO = {"id_solicitacao_cliente":"54834708","0":"54834708","carteira":"Santander NPL1","1":"Santander NPL1","origem":"A","2":"A","agencia_fornecedor":"Ferreira e Chagas Advogados","3":"Ferreira e Chagas Advogados","devedor":"RONALDO CABRAL DUTRA,","4":"RONALDO CABRAL DUTRA,","nro_operacao_sistema":"8492206","5":"8492206","nro_operacao":"2290000172180-32-0424","6":"2290000172180-32-0424","setor":"PCJ","7":"PCJ","tipo":"GASTO","8":"GASTO","autorizacao":"","9":"","nro_despesa":"","10":"","data_gasto":"2018-01-24","11":"2018-01-24","tipo_gasto":"NC_Correios","12":"NC_Correios","moeda":"R$","13":"R$","valor":"19.70","14":"19.70","status_pagamento":"Carregado","15":"Carregado","data_aprovacao":"2018-02-14","16":"2018-02-14","id_juizo":"","17":"","id_reclamo":"\r\n","18":"\r\n","cnpj":"22222222222","19":"22222222222"}
 
 const cloudant = Cloudant({
   "username": "96ba32ad-e17d-494f-a93e-72240b1e0b16-bluemix",
@@ -15,7 +15,7 @@ const cloudant = Cloudant({
   "url": "https://96ba32ad-e17d-494f-a93e-72240b1e0b16-bluemix:e373c010bcb53c3ea89a59f7fa2642789e7bfe3128bab1c3e2762b713ab04641@96ba32ad-e17d-494f-a93e-72240b1e0b16-bluemix.cloudant.com"
 })
 
-function processRuleValidator(wksResponse, dadoSolicitacao){
+function processRuleValidator(wksResponse, dadosSolicitacao){
 
   return new Promise((resolve, reject) => {
 
@@ -23,7 +23,13 @@ function processRuleValidator(wksResponse, dadoSolicitacao){
 
     console.log('['+arguments.callee.name+'] Processando ...')
     console.log('==============================================')
-    console.log(dadoSolicitacao)
+    
+    if(dadosSolicitacao == undefined){
+      console.log('dadosSolicitacao nao recebidos ... Usando MOCK !!!')
+      console.log('+++++++++++++++++++++++++++++++++++++=')
+      dadosSolicitacao = MOCK_SOLICITACAO
+    }
+
     console.log('==============================================')
     
     promisify(dokia.view)('field', 'field-view').then((resultView)=>{
@@ -80,13 +86,13 @@ function processRuleValidator(wksResponse, dadoSolicitacao){
         })
 
         // Recupera os ID de regra referentes aos dados de solicitação
-        Object.keys(MOCK_SOLICITACAO).forEach((solicData, solicIndex) => {
+        Object.keys(dadosSolicitacao).forEach((solicData, solicIndex) => {
 
           if(solicData == value.title){
 
              inputs.push({
               idField: value._id,
-              value: MOCK_SOLICITACAO[solicData]
+              value: dadosSolicitacao[solicData]
             })
 
           }
